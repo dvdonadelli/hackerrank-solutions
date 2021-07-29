cf620cb0e9fe6cef0d7feb16a0b748566fbc780c
Davi <davi_donadelli@live.com>
2021-07-29 15:39:20 -0300

    Changes

--- src/main/java/br/com/hackerrank/solutions/warmup/CountingValleys.java
+++ src/main/java/br/com/hackerrank/solutions/warmup/CountingValleys.java
@@ -2,6 +2,11 @@ package br.com.hackerrank.solutions.warmup;
 
 public class CountingValleys {
 
+    // Parameters
+    // n = The amount of steps the Hiker made
+    // path = All the path {U for Up, D for Down}
+    // Expected output
+    // A number representing the number of valley's he's been through
     public static int countingValleys(int n, String path) {
         int altitude = 0, valleys = 0;
         for (int i = 0; i < n; i++){
